4707837f42c245399f38f3dab144fe7b989b4abb
ramzi796 <ramzi796@gmail.com>
2025-04-30 19:38:44 +0530

    Auth flow completed

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:root@localhost/mythos360")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:root@localhost/mythosDB")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 from routers import auth
 from database import engine, Base
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # Replace with your frontend port
+    allow_credentials=True,
+    allow_methods=["*"],                      # <-- Must allow OPTIONS
+    allow_headers=["*"],
+)
 
 Base.metadata.create_all(bind=engine)
 
